7149288e53892fcbf49e39febafbf7d4ad8b1a98
Marcio Klepacz <marcioklepacz@gmail.com>
2019-07-22 14:22:48 -0300

    Add new source

--- lib/sampamotors.rb
+++ lib/sampamotors.rb
@@ -13,13 +13,48 @@ module Sampamotors
         {
           email_id: 'jivochat',
           name: 'JivoChat'
-        }
+        },
+        {
+          email_id: 'website',
+          name: 'Website - Novos'
+        },
       ]
     end
   end
 
   class F1SalesCustom::Email::Parser
     def parse
+      destinatary = @email.to.map { |email| email[:email].split('@').first } 
+
+      if destinatary.include?('jivochat')
+        parse_jivochat
+      elsif destinatary.include?('website')
+        parse_website
+      end
+    end
+
+    private 
+
+    def parse_website
+      parsed_email = @email.body.colons_to_hash
+      message = parsed_email['mensagem'].split("\n").first
+      message += "\n#{parsed_email['loja']}"
+
+      {
+        source: {
+          name: F1SalesCustom::Email::Source.all[1][:name],
+        },
+        customer: {
+          name: parsed_email['nome'],
+          phone: parsed_email['telefone'].tr('^0-9', ''),
+          email: parsed_email['email']
+        },
+        product: parsed_email['produto'],
+        message: message,
+      }
+    end
+
+    def parse_jivochat
       parsed_email = @email.body.colons_to_hash(/(nome|email|telefone|produto|mensagem).*?:/, false)
 
       {
@@ -34,7 +69,6 @@ module Sampamotors
         product: parsed_email['produto'],
         message: parsed_email['mensagem'],
       }
-
     end
   end  
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,6 +5,40 @@ require "f1sales_custom/source"
 RSpec.describe F1SalesCustom::Email::Parser do
 
   context 'when email is from website form' do
+    let(:email) do
+      email = OpenStruct.new
+      email.to = [email: 'website@lojateste.f1sales.org']
+      email.subject = 'Campanha - CITY e CIVIC'
+      email.body = "Contato via site\n*Nome:* Regina Rodrigues da Cruz\n*E-mail:* jhonatancruz@uol.com.br\n*Telefone:* (11) 9 6216-0607\n*Loja:* Ponte Piqueri\n*Mensagem:* Possível cliente PCD\n-----------------------------------------------------------------------\n*Link da Land:* promocao.sampamotors.com.br/julho/\n*UTM Source:* facebook\n*UTM Medium:* link\n*Mensagem de e-mail confidencial.*"
+
+      email
+    end
+
+    let(:parsed_email) { described_class.new(email).parse }
+
+    it 'contains website form as source name' do
+      expect(parsed_email[:source][:name]).to eq(F1SalesCustom::Email::Source.all[1][:name])
+    end
+
+    it 'contains name' do
+      expect(parsed_email[:customer][:name]).to eq('Regina Rodrigues da Cruz')
+    end
+
+    it 'contains email' do
+      expect(parsed_email[:customer][:email]).to eq('jhonatancruz@uol.com.br')
+    end
+
+    it 'contains phone' do
+      expect(parsed_email[:customer][:phone]).to eq('11962160607')
+    end
+
+    it 'contains message' do
+      expect(parsed_email[:message]).to eq("Possível cliente PCD\nPonte Piqueri")
+    end
+
+  end
+
+  context 'when email is from jivochat' do
     let(:email){ 
       email = OpenStruct.new
       email.to = [email: 'jivochat@lojateste.f1sales.org']
